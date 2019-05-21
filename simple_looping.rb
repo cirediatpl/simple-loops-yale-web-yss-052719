c5c6e54c0ca52744b76cc3bf6cf532855eb7b1a5
colorful-architect-9266 <nam-joong.hwang@yale.edu>
2019-05-21 19:29:15 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,7 @@ end
 def while_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  while counter < 8
+  while counter < 7
     puts phrase
     counter += 1
   end
@@ -31,7 +31,7 @@ end
 def until_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  until counter == 8
+  until counter == 7
     puts phrase
     counter += 1
   end
